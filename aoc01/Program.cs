ab03e18070525355964e1ed4d758060a7d9480eb
Greg Tan <greg.tan@firstmode.com>
2023-12-02 12:24:59 +0800

    Day 1 part 1

---
+++ aoc01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var inputOption = new Option<string>(
+            name: "--input",
+            description: "Input file path",
+            getDefaultValue: () => "input.txt");
+
+        var rootCommand = new RootCommand();
+        rootCommand.AddOption(inputOption);
+
+        rootCommand.SetHandler(Run, inputOption);
+
+        await rootCommand.InvokeAsync(args);
+    }
+
+    private static async Task Run(string input)
+    {
+        var lines = File.ReadLinesAsync(input);
+        var numbers = lines
+            .Select(line =>
+            {
+                var nums = line.Where(c => char.IsDigit(c)).ToArray();
+                return int.Parse($"{nums[0]}{nums[^1]}");
+            });
+
+        var sum = numbers.ToEnumerable().Sum();
+        Console.WriteLine(sum);
+    }
+}
+
+
