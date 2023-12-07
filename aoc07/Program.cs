6b52fe3a5a130050b37925645d5fb8088dea69d9
Greg Tan <greg.tan@firstmode.com>
2023-12-07 22:23:01 +0800

    Day 7 part 2

--- aoc07/Program.cs
+++ aoc07/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.CommandLine;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class Program
 {
@@ -25,20 +26,22 @@ public partial class Program
 
     public static async Task Run(string input, int part)
     {
-        var hands = await Parse(input);
+        var bids = await Parse(input);
         var task = part switch
         {
-            1 => Part1(hands),
-            2 => Part2(),
+            1 => Part1(bids),
+            2 => Part2(bids),
             _ => throw new ArgumentOutOfRangeException(nameof(part))
         };
 
         await task;
     }
 
-    private static Task Part1(IEnumerable<Hand> hands)
+    private static Task Part1(IEnumerable<(char[] Cards, int Bid)> bids)
     {
-        var scoredHands =  hands.Order()
+        var hands = bids.Select(b => new Hand(b.Cards, StrengthCalculator.Calculate(b.Cards), b.Bid));
+
+        var scoredHands =  hands.Order(new StandardComparer())
             .Select((h, i) => (Hand: h, Winnings: (i + 1) * h.Bid));
 
         var totalWinnings = scoredHands.Select(s => s.Winnings).Sum();
@@ -48,78 +51,87 @@ public partial class Program
         return Task.CompletedTask;
     }
 
-    private static Task Part2()
+    private static Task Part2(IEnumerable<(char[] Cards, int Bid)> bids)
     {
+        var hands = bids.Select(b => new Hand(b.Cards, StrengthCalculator.CalculateJokerStrength(b.Cards), b.Bid));
+
+        var scoredHands =  hands.Order(new JokerComparer())
+            .Select((h, i) => (Hand: h, Winnings: (i + 1) * h.Bid));
+
+        var totalWinnings = scoredHands.Select(s => s.Winnings).Sum();
+
+        Console.WriteLine(totalWinnings);
+
         return Task.CompletedTask;
     }
 
-    private static async Task<List<Hand>> Parse(string input)
+    private static async Task<List<(char[] Cards, int Bid)>> Parse(string input)
     {
         return await File.ReadLinesAsync(input)
             .Select(line =>
             {
                 var parts = line.Split(' ');
-                return new Hand(parts[0], Convert.ToInt32(parts[1]));
+                return (parts[0].ToCharArray(), Convert.ToInt32(parts[1]));
             })
             .ToListAsync();
     }
 }
 
-public class Hand : IComparable<Hand>
+public record Hand (char[] Cards, Strength Strength, int Bid);
+
+public static class StrengthCalculator
 {
-    private static Dictionary<char, int> CardValues = new()
+    private static readonly char[] NonJokerCards = "23456789TQKA".ToCharArray();
+
+    public static Strength CalculateJokerStrength(char[] cards)
     {
-        ['A'] = 12,
-        ['K'] = 11,
-        ['Q'] = 10,
-        ['J'] = 9,
-        ['T'] = 8,
-        ['9'] = 7,
-        ['8'] = 6,
-        ['7'] = 5,
-        ['6'] = 4,
-        ['5'] = 3,
-        ['4'] = 2,
-        ['3'] = 1,
-        ['2'] = 0,
-    };
+        var possibleHands = CalculatePossibleHands(cards);
 
-    public Hand (string cards, int bid)
-    {
-        Cards = cards.ToCharArray();
-        Bid = bid;
-        Strength = CalculateStrength(Cards);
-    }
-
-    public char[] Cards { get; init; }
-
-    public int Bid;
-
-    public Strength Strength { get; init; }
-
-    public int CompareTo(Hand other)
-    {
-        var ordinal = Strength - other.Strength;
-        
-        if (ordinal != 0)
+        var bestHand = Strength.HighCard;
+        foreach (var hand in possibleHands)
         {
-            return ordinal;
-        }
-
-        for (var i = 0; i < Cards.Length; i++)
-        {
-            ordinal = CardValues[Cards[i]] - CardValues[other.Cards[i]];
-            if (ordinal != 0)
+            var strength = Calculate(hand);
+            if (strength > bestHand)
             {
-                return ordinal;
+                bestHand = strength;
+            }
+            if (bestHand == Strength.FiveOfAKind)
+            {
+                break;
             }
         }
-
-        return 0;
+        return bestHand;
     }
 
+    private static List<char[]> CalculatePossibleHands(char[] cards)
+    {
+        var cardsCopy = new char[cards.Length];
+        Array.Copy(cards, cardsCopy, cards.Length);
+        var jokerPositions = Enumerable.Range(0, cards.Length)
+            .Where(i => cards[i] == 'J');
 
-    public static Strength CalculateStrength(char[] cards)
+        var possibleHands = new List<char[]>([cardsCopy]);
+        
+        foreach (var i in jokerPositions)
+        {
+            var newHands = new List<char[]>();
+            foreach (var c in possibleHands)
+            {
+                foreach (var newCard in NonJokerCards)
+                {
+                    var newHand = new char[c.Length];
+                    Array.Copy(c, newHand, c.Length);
+                    newHand[i] = newCard;
+                    newHands.Add(newHand);
+                }
+            }
+            possibleHands = newHands;
+        }
+
+        return possibleHands;
+    }
+
+    public static Strength Calculate(char[] cards)
     {
         var counts = new Dictionary<char, int>();
         foreach (var c in cards)
@@ -163,6 +175,88 @@ public class Hand : IComparable<Hand>
         return Strength.HighCard;
     }
 
+}
+
+public class StandardComparer : IComparer<Hand>
+{
+    private static readonly Dictionary<char, int> CardValues = new()
+    {
+        ['2'] = 0,
+        ['3'] = 1,
+        ['4'] = 2,
+        ['5'] = 3,
+        ['6'] = 4,
+        ['7'] = 5,
+        ['8'] = 6,
+        ['9'] = 7,
+        ['T'] = 8,
+        ['J'] = 9,
+        ['Q'] = 10,
+        ['K'] = 11,
+        ['A'] = 12,
+    };
+
+    public int Compare(Hand x, Hand y)
+    {
+        var ordinal = x.Strength - y.Strength;
+        
+        if (ordinal != 0)
+        {
+            return ordinal;
+        }
+
+        for (var i = 0; i < x.Cards.Length; i++)
+        {
+            ordinal = CardValues[x.Cards[i]] - CardValues[y.Cards[i]];
+            if (ordinal != 0)
+            {
+                return ordinal;
+            }
+        }
+
+        return 0;
+    }
+}
+
+public class JokerComparer : IComparer<Hand>
+{
+    private static readonly Dictionary<char, int> CardValues = new()
+    {
+        ['J'] = 0,
+        ['2'] = 1,
+        ['3'] = 2,
+        ['4'] = 3,
+        ['5'] = 4,
+        ['6'] = 5,
+        ['7'] = 6,
+        ['8'] = 7,
+        ['9'] = 8,
+        ['T'] = 9,
+        ['Q'] = 10,
+        ['K'] = 11,
+        ['A'] = 12,
+    };
+
+    public int Compare(Hand x, Hand y)
+    {
+        var ordinal = x.Strength - y.Strength;
+        
+        if (ordinal != 0)
+        {
+            return ordinal;
+        }
+
+        for (var i = 0; i < x.Cards.Length; i++)
+        {
+            ordinal = CardValues[x.Cards[i]] - CardValues[y.Cards[i]];
+            if (ordinal != 0)
+            {
+                return ordinal;
+            }
+        }
+
+        return 0;
+    }
 }
 
 public enum Strength
