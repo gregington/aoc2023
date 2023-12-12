b96e4b47d42efe73c24c199f30a4388df4a0d03f
Greg Tan <greg.tan@firstmode.com>
2023-12-12 20:38:36 +0800

    Day 12 part 2

--- aoc12/Program.cs
+++ aoc12/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections;
+using System.Collections.Immutable;
 using System.CommandLine;
+using System.ComponentModel;
 using System.Text.RegularExpressions;
 
 public partial class Program
@@ -40,7 +42,7 @@ public partial class Program
 
     private static Task Part1(ImmutableArray<Springs> springsList)
     {
-        var count = springsList.Select(s => PossibleConditions(s.Condition).Where(c => Match(c, s.DamagedCounts)).Count()).Sum();
+        var count = springsList.Select(s => Process(s.Condition, s.DamagedCounts)).Sum();
 
         Console.WriteLine(count);
 
@@ -49,8 +51,100 @@ public partial class Program
 
     private static Task Part2(ImmutableArray<Springs> springsList)
     {
+        var unfolded = springsList.Select(x => Unfold(x, 5));
+
+        var completed = 0;
+        var total = 0L;
+
+        Parallel.ForEach(unfolded, springs =>
+        {
+            var count = Process(springs.Condition, springs.DamagedCounts);
+
+            var localCompleted = Interlocked.Increment(ref completed);
+            var localTotal = Interlocked.Add(ref total, count);
+
+            Console.WriteLine($"{localTotal,8} ({localCompleted,4}/{springsList.Length,4})");
+        });
+
         return Task.CompletedTask;
     }
+
+    private static Springs Unfold(Springs input, int count)
+    {
+        var condition = string.Join('?', Enumerable.Range(0, count).Select(_ => input.Condition));
+
+        var damagedCounts =  new List<int>();
+
+        for (var i = 0; i < count; i++)
+        {
+            damagedCounts.AddRange(input.DamagedCounts);
+        }
+
+        return new Springs(condition, [.. damagedCounts]);
+    }
+
+    private static long Process(string springs, ImmutableArray<int> damagedCounts)
+    {
+        return Process(springs, [.. damagedCounts], []);
+    }
+
+    private static long Process(string springs, List<int> damagedCounts, Dictionary<string, long> cache)
+    {
+        var key = $"{springs}-{string.Join(',', damagedCounts.Select(x => x.ToString()))}";
+        if (cache.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+
+        value = Count(springs, damagedCounts, cache);
+        cache[key] = value;
+        return value;
+    }
+
+    private static long Count(string springs, List<int> damagedCounts, Dictionary<string, long> cache)
+    {
+        return springs.FirstOrDefault() switch {
+            '.' => HandleWorking(springs, damagedCounts, cache),
+            '#' => HandleDamaged(springs, damagedCounts, cache),
+            '?' => HandleUnknown(springs, damagedCounts, cache),
+            _ => HandleEnd(damagedCounts)
+        };
+    }
+
+    private static long HandleWorking(string springs, List<int> damagedCounts, Dictionary<string, long> cache) =>
+        Process(springs[1..], damagedCounts, cache);
+
+    private static long HandleDamaged(string springs, List<int> damagedCounts, Dictionary<string, long> cache)
+    {
+        if (damagedCounts.Count == 0)
+        {
+            return 0;
+        }
+
+        var c = damagedCounts[0];
+        damagedCounts = damagedCounts[1..];
+
+        var leadingDamagedOrUnknown = springs.TakeWhile(x => x is '#' or '?').Count();
+
+        if (leadingDamagedOrUnknown < c)
+        {
+            return 0;
+        }
+        if (springs.Length == c)
+        {
+            return Process("", damagedCounts, cache);
+        }
+        if (springs[c] == '#')
+        {
+            return 0;
+        }
+        return Process(springs[(c+1)..], damagedCounts, cache);
+    }
+
+    private static long HandleUnknown(string springs, List<int> damagedCounts, Dictionary<string, long> cache) =>
+        Process("." + springs[1..], damagedCounts, cache) + Process("#" + springs[1..], damagedCounts, cache);
+
+    private static long HandleEnd(List<int> damagedCounts) => damagedCounts.Count == 0 ? 1 : 0;
 
     private static bool Match(string conditions, ImmutableArray<int> damagedCounts)
     {
